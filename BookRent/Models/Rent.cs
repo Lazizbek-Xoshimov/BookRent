838a9d992fa5c7ae0b65f50ef6ea4dd598bc921d
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2025-01-19 17:03:27 +0500

    Add project files.

---
+++ BookRent/Models/Rent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRent.Models
+{
+    public class Rent
+    {
+        public int RentId { get; set; }
+        public User User { get; set; }
+        public Book Book { get; set; }
+        public DateTime GivenTime { get; set; }
+        public DateTime ReturnedTime {  get; set; }
+        public bool IsReturned { get; set; }
+    }
+}
---
+++ BookRent/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRent.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        public UserType UserType { get; set; }
+    }
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    public enum UserType
+    {
+        Student,
+        Teacher,
+        Librariant
+    }
+}
