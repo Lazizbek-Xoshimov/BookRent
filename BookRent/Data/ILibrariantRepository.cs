ba000317f3bbe60eff10f21a520b6b6f5eb1bded
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2025-01-24 19:34:20 +0500

    create data

---
+++ BookRent/Data/ILibrariantRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookRent.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRent.Data
+{
+    public interface ILibrariantRepository
+    {
+        List<User> SelectAllLibrariants();
+        User SelectLibrariantById(int librariantId);
+        User InsertLibrariant(User librariant);
+        User UpdateLibrariant(int librariantId, User librariant);
+        bool DeleteLibrariant(int librariantId);
+    }
+}
---
+++ BookRent/Data/LibrariantRepository.cs
@@ -0,0 +1,72 @@
+﻿using BookRent.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRent.Data
+{
+    public class LibrariantRepository : ILibrariantRepository
+    {
+        private Dictionary<int, User> librariants;
+
+        public LibrariantRepository()
+        {
+            this.librariants = new Dictionary<int, User>();
+        }
+
+        public User SelectLibrariantById(int librariantId)
+        {
+            if (!librariants.ContainsKey(librariantId))
+                throw new KeyNotFoundException("Librariant not found.");
+
+            return librariants[librariantId];
+            
+        }
+
+        public List<User> SelectAllLibrariants() =>
+             librariants.Values.ToList();
+
+        public User InsertLibrariant(User librariant)
+        {
+            if (!librariants.ContainsKey(librariant.UserId))
+                throw new ArgumentException("Librariant with this key already exists.");
+
+            librariants.Add(librariant.UserId, librariant);
+
+            return librariant;
+        }
+
+        public User UpdateLibrariant(int librariantId, User librariant)
+        {
+            if (!librariants.ContainsKey(librariantId))
+                throw new KeyNotFoundException("Librariant not found.");
+
+            var exictingLibrariant = librariants[librariantId];
+
+            if(string.IsNullOrEmpty(librariant.Name))
+                throw new ArgumentNullException("librariant.Name cannot be null.");
+
+            exictingLibrariant.Name = librariant.Name;
+            
+            exictingLibrariant.Gender = librariant.Gender;
+
+            if(librariant.BirthDate != DateTime.Now)
+                throw new Exception("BirthDate cannot be DateTime.Now");
+
+            exictingLibrariant.BirthDate = DateTime.Now;
+
+            exictingLibrariant.Type = librariant.Type;
+
+            return exictingLibrariant;
+        }
+        public bool DeleteLibrariant(int librariantId)
+        {
+            if (!librariants.ContainsKey(librariantId))
+                throw new KeyNotFoundException("Librariant not found.");
+
+            return librariants.Remove(librariantId);
+        }
+    }
+}
